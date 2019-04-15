1bb1874ba10eb99f6caefe60a2f2c695e89a0835
westermann920 <42929525+westermann920@users.noreply.github.com>
2019-04-15 13:01:37 +0200

    setup

---
+++ Assets/Brilleaberne/Scripts/Node.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+    public string name;
+    public string molecule;
+    public int AtomicNumber;
+    public int MassNumber;
+    public int Protons;
+    public int Neutrons;
+    public int Electrons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Brilleaberne/Scripts/SpawnCanvas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCanvas : MonoBehaviour
+{
+    public GameObject prefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LineRenderer line = gameObject.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)) {
+            
+        }
+    }
+
+  public void SpawnCanvasFunction() {
+        Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+    }
+}
