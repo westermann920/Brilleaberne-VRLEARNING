1dfd4b63c4d8321e6818395f23b4767dfc590f17
westermann920 <benjaminwestermann89@gmail.com>
2019-05-15 16:04:32 +0200

    Small test
    -Split molecule Menu and Text
    -made bad temp to spawn Text

--- Assets/Brilleaberne/Scripts/SpawnCanvas.cs
+++ Assets/Brilleaberne/Scripts/SpawnCanvas.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class SpawnCanvas : MonoBehaviour
 {
-    public GameObject prefab;
+    public GameObject prefabMolMenu;
+    public GameObject prefabMolText;
     public GameObject laser;
     public Text CanvasMoleculeText;
     GameObject hitObj;
@@ -37,24 +38,51 @@ public class SpawnCanvas : MonoBehaviour
 
     public void SpawnCanvasFunction()
     {
-        H = 0;
-        O = 0;
-        LinkedList<string> MoleculeList = hitObj.GetComponent<NodeList>().GetMoleculeList();
-        Debug.Log("Length of moleculeList " + MoleculeList.Count);
-        while (MoleculeList.Count > 0) {
-            if (MoleculeList.Contains("H"))
-            {
-                H++;
-                MoleculeList.Remove("H");
+        if (hitObj.GetComponent<Node>()) {
+            H = 0;
+            O = 0;
+
+            //int letterCounter = 2;
+
+            LinkedList<string> MoleculeList = hitObj.GetComponent<NodeList>().GetMoleculeList();
+            Debug.Log("Length of moleculeList " + MoleculeList.Count);
+            while (MoleculeList.Count > 0) {
+                if (MoleculeList.Contains("H"))
+                {
+                    H++;
+                    MoleculeList.Remove("H");
+                }
+                else if (MoleculeList.Contains("O"))
+                {
+                    O++;
+                    MoleculeList.Remove("O");
+                }
             }
-            else if (MoleculeList.Contains("O"))
-            {
-                O++;
-                MoleculeList.Remove("O");
+            Debug.Log("H" + H + " O" + O);
+            //CanvasMoleculeText.text = "Molecule:" + '\n' + "H" + H + " O" + O;
+
+            //
+            GameObject moleculeMenu = Instantiate(prefabMolMenu, new Vector3(hitObj.transform.position.x + 0.5f, hitObj.transform.position.y, hitObj.transform.position.z), Quaternion.identity);
+            ArrayList letterCounter = new ArrayList();
+            letterCounter.Add("H");
+            letterCounter.Add("O");
+
+            if(H > 0)
+            { 
+                GameObject moleculeText = Instantiate(prefabMolText, new Vector3(moleculeMenu.transform.position.x, moleculeMenu.transform.position.y, moleculeMenu.transform.position.z), Quaternion.identity);
+                moleculeText.GetComponent<Text>().text = "H" + H;
+                moleculeText.transform.SetParent(moleculeMenu.transform);
             }
+            if (O > 0)
+            {
+                GameObject moleculeText = Instantiate(prefabMolText, new Vector3(moleculeMenu.transform.position.x + 1, moleculeMenu.transform.position.y, moleculeMenu.transform.position.z), Quaternion.identity);
+                moleculeText.GetComponent<Text>().text = "O" + O;
+                moleculeText.transform.SetParent(moleculeMenu.transform);
+            }
+
+
+
+            //moleculeMenu.GetComponentInChildren<Text>().text = "Molecule:" + '\n' + "H" + H + " O" + O;
         }
-        Debug.Log("H" + H + " O" + O);
-        CanvasMoleculeText.text = "Molecule:" + '\n' + "H" + H + " O" + O;
-        //Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
     }
 }
